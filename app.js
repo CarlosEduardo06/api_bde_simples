3f89b0df2c49b35371e426bc1816c3c1bf3d4b3a
calixto <calixtoreis06@outlook.com>
2024-08-19 12:04:12 -0300

    Adicionando o app.listen denovo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("node:path");
 const rootPage = require(path.resolve(__dirname, "routes/index"));
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 require(path.resolve(__dirname, "config/database"));
 const app = express();
 
@@ -10,3 +10,9 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 app.use("/", rootPage);
+
+app.listen(port, () => {
+  console.log(`
+listen on port: ${port}
+  `);
+});
